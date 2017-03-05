1a51feb51f2acafc8cdd049360426a61b0fa3657
Edgar Albalate <dev.gardo@gmail.com>
2017-03-05 09:39:16 +0100

    Added first usability. Started commands.

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 #!/usr/bin/env node
 
-console.log("Heloo");
+const program = require("commander");
+
+program
+  .version('0.0.0')
+  .option('-t, --test', 'Starts a test')
+
+  .parse(process.argv);
+
+console.log("INDEX");
+if (program.test) console.log('  - Doing a test');
